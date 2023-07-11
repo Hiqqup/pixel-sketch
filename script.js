b6c974484c1349c0322b4882d88b60ad906f284d
Hiqqup <jubu6000@gmail.com>
2023-07-11 18:41:08 +0200

    Add default brush feature

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.querySelector('.canvas');
+generateGrid(16, 16);
+
+function generateGrid(width, height){
+    for(let i =0; i<height;i++){
+        const row = document.createElement('div');
+        row.classList.add('gridRow');
+        for(let j = 0; j<width;j++){
+            const gridEl = document.createElement('div');
+            gridEl.classList.add('gridEl');
+            gridEl.addEventListener('mousedown', changeColor);
+            gridEl.addEventListener('mouseover', changeColor);
+            row.appendChild(gridEl);
+        }
+        canvas.appendChild(row);
+    }
+}
+function changeColor(e){
+    if (e.type === 'mouseover' && e.buttons==1 || e.type === 'mousedown'){
+        e.target.style.backgroundColor = 'black';
+    }
+    
+}
